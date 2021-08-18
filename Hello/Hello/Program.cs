f11d1db0ebc6bfd1636bd155264dc8d66c88116a
AndresITHS <89127120+AndresITHS@users.noreply.github.com>
2021-08-18 10:04:54 +0200

    Inlämning

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my name is Andres!");
+        }
+    }
+}
